2d50bf74dd1d46dda701d020b3f8ae2b4d1662ca
git <git@f3e199e4c16e.vps.myjino.ru>
2023-07-26 17:43:00 +0300

    roll_d20.py

--- d20/roll_d20.py
+++ d20/roll_d20.py
@@ -1,2 +1,9 @@
 import d20
-print(str(d20.roll("1d20+5")))
+while True:
+    print('input what to roll')
+    print('templates: "d20" "1d20" "3d4+3" "1d8+1d6+3"')
+    print('input "stop" or send empty message to stop')
+    answer = input()
+    print(str(d20.roll(answer)))
+    if answer == '' or answer == 'stop':
+        break
